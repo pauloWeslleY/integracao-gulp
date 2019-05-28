31994494b01a217669b21487268a69ec2842263e
Weslley Lima <weslleyspfc13@outlook.com>
2019-05-28 10:31:18 -0300

    curso-typescript

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const { series, parallel, src, dest } = require ('gulp')
+const del = require('del')
+const browserify = require('browserify')
+const source = require('vinyl-source-stream')
+const tsify = require('tsify')
+
+
+function limparDist() {
+   return del(['dist'])
+}
+
+function copiarHTML(cb) {
+   return src('public/**/*')
+      .pipe(dest('dist'))
+}
+
+function gerarJS(cb) {
+   return browserify({
+      basedir: '.',
+      entries: ['src/main.ts']
+   })
+      .plugin(tsify)
+      .bundle()
+      .pipe(source('app.js'))
+      .pipe(dest('dist'))
+}
+
+exports.default = series( 
+   limparDist,
+   parallel(gerarJS, copiarHTML)
+)
